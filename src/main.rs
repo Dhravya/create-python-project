ac0f287e810e129b6044879401dffbe237394874
Dhravya <dhravyashah@gmail.com>
2022-03-06 23:41:13 +0530

    added more

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
 
     println!("Successfully created the virual environment. You need to run the command {} to activate the venv", activate_command);
 
+    println!("Initializing git repository");
     let git_init = Command::new("git")
         .arg("init")
         .output()
@@ -23,6 +24,7 @@ fn main() {
 
     println!("{}", String::from_utf8_lossy(&git_init.stdout));
 
+    println!("Creating gitignore file");
     let mut gitignore_file =
         std::fs::File::create(".gitignore").expect("Failed to create gitignore file");
 
@@ -30,11 +32,16 @@ fn main() {
     gitignore_file
         .write_all(gitignore_content.as_bytes())
         .expect("Failed to write to gitignore file");
+    println!("Gitignore successfully created");
 
+    println!("Creating requirements file...");
     std::fs::File::create("requirements.txt").expect("Failed to create requirements file");
+    println!("Successfully created requirements file");
 
+    println!("Creating source directoryy...");
     std::fs::create_dir("src").expect("Failed to create src directory");
 
+    println!("Creating main.py file...");
     let mut main_file = std::fs::File::create("src/main.py").expect("Failed to create main file");
     let main_file_content = r#"
 # Imports here
@@ -50,4 +57,16 @@ if __name__ == "__main__":
     main_file
         .write_all(main_file_content.as_bytes())
         .expect("Failed to write to main file");
+
+    println!("Successfully created main.py file");
+
+    println!("Creating README.md file...");
+    std::fs::File::create("README.md").expect("Failed to create readme file");
+    
+    println!("Successfully created README.md file");
+
+    println!("Creating .env file...");
+    std::fs::File::create(".env").expect("Failed to create .env file");
+
+    println!("Successfully initialised python project");
 }
